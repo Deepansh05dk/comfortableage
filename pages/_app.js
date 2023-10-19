191e6f88352f6ff09baee2c350734f2f19e128dc
Deepansh05dk <84671261+Deepansh05dk@users.noreply.github.com>
2023-10-19 23:38:01 +0530

    designed product and orders page

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import { DataProvider } from "@/context/DataProvider";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <DataProvider>
+      <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </DataProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -17,9 +18,7 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Navbar />
       <main className="bg-red-500">this is body</main>
-      <Footer />
     </>
   );
 }
